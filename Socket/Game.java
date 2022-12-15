997111032dbadce74b1436bd0b13d0a60a7590e7
tsii06 <120597032+tsii06@users.noreply.github.com>
2022-12-15 06:43:49 +0100

    Add files via upload

---
+++ Socket/Game.java
@@ -0,0 +1,73 @@
+
+package panel;
+import client.Client;
+import listener.Listener;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.lang.Exception;
+import java.util.Vector;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.*;
+
+
+public class Game extends JPanel{
+	Matrice m=new Matrice();
+	Cellule [][] cellule=new Cellule[15][15];
+	Dimension ech=new Dimension(750,750);
+	int celluleTaille=50;
+	Border lineBorder = BorderFactory.createLineBorder(Color.BLACK,1);
+	Listener list;
+	Client c;
+	public Game(Client c){  
+		this.c=c;
+		this.setFocusable(true);
+		this.setPreferredSize(ech);
+		this.setBackground(Color.black);
+		this.setLayout(new GridLayout(15,15));
+		list=new Listener(m,cellule,this,c);
+		this.addMouseListener(list);
+        for (int i = 0; i < m.getMatrice().length; i++) {
+            for (int j = 0; j < m.getMatrice()[i].length; j++) {	
+            	cellule[i][j]=new Cellule();              
+            }
+        }
+	}	
+	public Matrice getMatrice(){
+		return m;
+	}
+	public Cellule [][] getCellulle(){
+		return cellule;
+	}
+	public Listener getListener(){
+		return list;
+	}
+	public void paint(Graphics g) {
+		super.paint(g);
+		this.draw(g);	
+		repaint();
+	}
+	public void draw(Graphics g){
+		for(int i=0;i<15;i++){
+			g.drawLine(0,i*50,0,750);
+		}
+		for (int i = 0; i < m.getMatrice().length; i++) {
+            for (int j = 0; j < m.getMatrice()[i].length; j++) {	
+            	cellule[i][j]=new Cellule();
+            	cellule[i][j].addMouseListener(list);
+                if(m.getMatrice()[i][j]==4){
+                	cellule[i][j].dessineBlue(g,i,j);
+                }
+                if(m.getMatrice()[i][j]==3){
+                	cellule[i][j].dessineRed(g,i,j);
+                }                
+                if(m.getMatrice()[i][j]==0 || m.getMatrice()[i][j]==1 || m.getMatrice()[i][j]==2){
+                	cellule[i][j].dessineClou(g,i,j);
+                }
+
+
+            }
+        }	
+	}
+
+}
---
+++ Socket/readme.txt
@@ -0,0 +1,4 @@
+Executer le server
+Dans ce jeu vous pouvez exectuter plusieurs clients
+Le but du jeu est de cocher le case rouge en evitant cocher le case bleu
+Quand vous cocher sur une case vide , ce case devient bleu
